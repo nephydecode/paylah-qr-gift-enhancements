ca771bbd31d290f2a6d04d5b23bc7eacab948221
lungkuangkoh <nephrite4@gmail.com>
2019-07-02 18:02:18 +0800

    dark background

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,65 +1,96 @@
-import React, { Component } from 'react';
-import Redemptions from './Redemptions';
-import { Container, Row, Col } from 'react-bootstrap';
-import './Welcome.css';
-import decowhite from '../assets/deco-white.svg';
-import decoblack from '../assets/deco-black.svg';
-import shadow from '../assets/shadow.svg';
-import largeqrgift from '../assets/largeqrgift.svg';
-import whitearrow from '../assets/arrow-button.svg';
-import { relative } from 'path';
+import React, { Component } from "react";
+import Redemptions from "./Redemptions";
+import { Container, Row, Col } from "react-bootstrap";
+import "./Welcome.css";
+import decowhite from "../assets/deco-white.svg";
+import decoblack from "../assets/deco-black.svg";
+import shadow from "../assets/shadow.svg";
+import largeqrgift from "../assets/largeqrgift.svg";
+import whitearrow from "../assets/arrow-button.svg";
+import { relative } from "path";
+import BgImage from "./dark.svg";
+
+var BgStyle = {
+  backgroundImage: `url(${BgImage})`,
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "bottom center",
+  backgroundSize: "contain",
+  height: "95vh",
+  backgroundColor: "#FA5A74"
+};
 
 class Welcome extends Component {
-    state = {};
-    render() {
-        return (
-            <div style={{ height: '100vh', backgroundColor: '#FA5A74' }}>
-                <Container>
-                    <Row className="pt-5">
-                        <Col xs={12}>
-                            <div className={'serialno'} style={{ fontColor: '#ffffff' }}>
-                                22jCoV
-                            </div>
-                        </Col>
-                    </Row>
-                    <Row className="p-4">
-                        <Col xs={12}>
-                            <div style={{ marginLeft: '-11em' }}>
-                                <img src={decowhite} style={{ position: 'absolute' }} className={'bobbingStars1'} />
-                                <img src={decoblack} style={{ position: 'absolute' }} className={'bobbingStars2'} />
-                            </div>
+  state = {};
+  render() {
+    return (
+      <div
+        style={BgStyle}
+        className={"d-flex flex-column justify-content-center"}
+      >
+        <Container>
+          <Row className="pt-5">
+            <Col xs={12}>
+              <div className={"serialno"} style={{ fontColor: "#ffffff" }}>
+                22jCoV
+              </div>
+            </Col>
+          </Row>
+          <Row className="p-4 pt-5">
+            <Col xs={12}>
+              <div style={{ marginLeft: "-11em" }}>
+                <img
+                  src={decowhite}
+                  style={{ position: "absolute" }}
+                  className={"bobbingStars1"}
+                />
+                <img
+                  src={decoblack}
+                  style={{ position: "absolute" }}
+                  className={"bobbingStars2"}
+                />
+              </div>
 
-                            <div>
-                                <img src={largeqrgift} style={{ position: '' }} className={'bobbingUpDown'} />
-                            </div>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={12}>
-                            <img src={shadow} className={'bobbingBigSmall'} />
-                        </Col>
-                    </Row>
-                    <Row className="m-3">
-                        <Col xs={12}>
-                            <h1 className={'header text-white'}>QR Gift</h1>
-                        </Col>
-                    </Row>
-                    <Row className="m-3">
-                        <Col xs={12}>
-                            <div className={'text text-white'}>
-                                You have Received a QR Gift! Choose one of the following methods to redeem your gift.
-                            </div>
-                        </Col>
-                    </Row>
-                    <Row className="pt-5">
-                        <Col xs={12} />
-                    </Row>
-                </Container>
+              <div>
+                <img
+                  src={largeqrgift}
+                  style={{ position: "" }}
+                  className={"bobbingUpDown"}
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <img src={shadow} className={"bobbingBigSmall"} />
+            </Col>
+          </Row>
+          <Row className="m-3">
+            <Col xs={12}>
+              <h1 className={"header text-white"}>QR Gift</h1>
+            </Col>
+          </Row>
+          <Row className="m-3">
+            <Col xs={12}>
+              <div className={"text text-white"}>
+                You have Received a QR Gift! Choose one of the following methods
+                to redeem your gift.
+              </div>
+            </Col>
+          </Row>
+          <Row className="p-4">
+            <Col xs={12} />
+          </Row>
+        </Container>
 
-                <img src={whitearrow} style={{ position: 'absolute', bottom: '2em' }} />
-            </div>
-        );
-    }
+        <div className={"d-flex justify-content-center"}>
+          <img
+            src={whitearrow}
+            style={{ position: "absolute", bottom: "2em" }}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Welcome;
