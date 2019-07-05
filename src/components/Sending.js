98248714150e33215b25c36929a39d23a6d7ac75
Lionell Yip <watonly.me@gmail.com>
2019-07-05 11:12:48 +0800

    Added animations bro

--- src/components/Sending.js
+++ src/components/Sending.js
@@ -1,88 +1,61 @@
-import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import cross from "../assets/cross.svg";
-import oval from "../assets/oval.svg";
-import lcloud from "../assets/envelope-left.svg";
-import rcloud from "../assets/envelope-right.svg";
-import flyplane from "../assets/flying-envel.svg";
-import "./Sending.css";
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import cross from '../assets/cross.svg';
+import oval from '../assets/oval.svg';
+import lcloud from '../assets/envelope-left.svg';
+import rcloud from '../assets/envelope-right.svg';
+import flyplane from '../assets/flying-envel.svg';
+import './Sending.css';
 
 class Sending extends Component {
-  state = {};
-  render() {
-    return (
-      <div
-        className={"d-flex flex-column justify-content-center main-screen-size"}
-      >
-        <Container className="">
-          <Row>
-            <Col>
-              <img
-                src={lcloud}
-                style={{ position: "absolute", zIndex: 999, marginTop: "-32%" }}
-                className="toprow"
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={9}>
-              <img src={cross} style={{ position: "absolute", zIndex: 999 }} />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={11} className="mt-2" />
-          </Row>
-          <Row>
-            <Col xs={9}>
-              <div
-                className="dot d-flex justify-content-center"
-                style={{
-                  position: "absolute",
-                  left: "32.25%",
-                  marginTop: "-25.5%"
-                }}
-              >
-                <div
-                  className="dot2"
-                  style={{
-                    position: "absolute",
-                    left: "25%",
-                    marginTop: "25%"
-                  }}
-                >
-                  <img
-                    src={flyplane}
-                    style={{
-                      position: "absolute",
-                      zIndex: 999,
-                      left: "-2.5%",
-                      marginTop: "20%"
-                    }}
-                  />
-                </div>
-              </div>
-            </Col>
-          </Row>
+    state = {};
+    render() {
+        return (
+            <div className={''}>
+                <Container className="">
+                    <Row>
+                        <Col xs={12} className={'d-flex flex-column justify-content-center sending-screen-size'}>
+                            <div className={'d-flex flex-row justify-content-center'}>
+                                <div className="dot mt-5" style={{}}>
+                                    <div
+                                        className="dot2"
+                                        style={{
+                                            position: 'relative',
+                                            top: '25%',
+                                        }}
+                                    >
+                                        <img
+                                            src={flyplane}
+                                            style={{
+                                                position: 'relative',
+                                                zIndex: 999,
+                                                left: '-2.5%',
+                                                marginTop: '20%',
+                                            }}
+                                            className={'plane-animation'}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+<div className={'pt-3'}><p className={'text-center'}>Help Lah!</p></div>
 
-          <Row>
-            <Col xs={12}>
-              <img src={oval} style={{ position: "absolute", zIndex: 999 }} />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <img
-                src={rcloud}
-                className="mt-5"
-                style={{ position: "absolute" }}
-              />
-              <div className={"d-block pb-5 mb-5-5"} />
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+                            <img src={rcloud} className="mt-5 base-animation base-animation-delay-11" style={{ position: 'absolute' , top: '10%'}} />
+                            <img src={lcloud} style={{ position: 'absolute', top: '30%', zIndex: 999 }} className="base-animation base-animation-delay-5" />
+                            <img src={cross} style={{ position: 'absolute', top: '40%', zIndex: 999 }} className={'base-animation base-animation-delay-6'} />
+                            <img src={cross} style={{ position: 'absolute',top: '44%', zIndex: 999 }} className={'base-animation base-animation-delay-1'} />
+                            <img src={cross} style={{ position: 'absolute',top: '60%', zIndex: 999 }} className={'base-animation base-animation-delay-7'} />
+                            <img src={oval} style={{ position: 'absolute', top: '65%',zIndex: 999 }} className={'base-animation base-animation-delay-2'} />
+                            <img src={oval} style={{ position: 'absolute', top: '42%', zIndex: 999 }} className={'base-animation base-animation-delay-3'} />
+                            <img src={oval} style={{ position: 'absolute', top: '58%', zIndex: 999 }} className={'base-animation base-animation-delay-8'}/>
+                            <img src={oval} style={{ position: 'absolute', top: '68%', zIndex: 999 }} className={'base-animation base-animation-delay-9'}/>
+                            <img src={rcloud} className="mt-5" style={{ position: 'absolute', top: '60%' }} className={'base-animation base-animation-delay-10'}/>
+                            <img src={lcloud} style={{ position: 'absolute', top: '70%', zIndex: 999 }} className={'base-animation base-animation-delay-4'}/>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default Sending;
